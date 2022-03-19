ecb5d89ae37b8213cca8b560335fabbfb77963f5
Matthiasclee <matthias@matthiasclee.com>
2022-03-19 05:42:11 -0700

    Added ability to move cursor twice with one line

--- rbtext.rb
+++ rbtext.rb
@@ -1,4 +1,5 @@
 require "./rbtext/cursor.rb"
+require "./rbtext/colors.rb"
 
 module RBText
 end
--- rbtext/cursor.rb
+++ rbtext/cursor.rb
@@ -1,21 +1,25 @@
 module RBText
   module Cursor
-    def up
-      print "\033[2A"
+    def up(num=1)
+      print "\033[#{num.to_i}A"
     end
 
-    def down
-      print "\033[2B"
+    def down(num=1)
+      print "\033[#{num.to_i}B"
     end
 
-    def left
-      print "\033[1D"
+    def left(num=1)
+      print "\033[#{num.to_i}D"
     end
 
-    def right
-      print "\033[1C"
+    def right(num=1)
+      print "\033[#{num.to_i}C"
     end
 
-    module_function :up, :down, :left, :right
+    def beginning_of_line
+      print "\r"
+    end
+
+    module_function :up, :down, :left, :right, :beginning_of_line
   end
 end
